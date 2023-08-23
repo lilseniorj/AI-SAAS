f51a71c67bda337b62bcdec2c8690824f16e9fa1
lilseniorj <jesusvarguer18@gmail.com>
2023-08-23 18:38:30 -0500

    Api Limit

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,8 +2,6 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
-import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
-
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -24,26 +22,18 @@ export async function POST(
       return new NextResponse("Prompt are required", { status: 400 });
     }
 
-    const freeTrial = await checkApiLimit();
-
-    if (!freeTrial) {
-      return new NextResponse("Free trial has expired.", { status: 403 });
-    }
-
     const response = await replicate.run(
-      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
         input: {
-          prompt_a: prompt
+          prompt
         }
       }
     );
 
-    await increaseApiLimit();
-
     return NextResponse.json(response);
   } catch (error) {
-    console.log("[MUSIC_ERROR]", error);
+    console.log("[VIDEO_ERROR]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
